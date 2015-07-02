f6ac1ed4bec68b96ca8807f3a129f2dab4940483
Katharine Berry <katharine@getpebble.com>
2015-07-02 02:36:54 -0700

    Added --version flag.

--- pebble_tool/__init__.py
+++ pebble_tool/__init__.py
@@ -9,11 +9,13 @@ from .commands.base import register_children
 from .commands import repl, install, screenshot, logs, account, timeline
 from .commands.sdk import build, emulator, create, convert
 from .exceptions import ToolError
+from .sdk import sdk_version
 
 
 def run_tool(args=None):
     logging.basicConfig()
     parser = argparse.ArgumentParser(description="Pebble Tool", prog="pebble")
+    parser.add_argument("--version", action="version", version="Pebble SDK {}".format(sdk_version()))
     register_children(parser)
     args = parser.parse_args(args)
     try:
