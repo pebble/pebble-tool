f80c05ee26fad9ceb90a4c8375a478341c303f26
Katharine Berry <katharine@getpebble.com>
2015-07-02 05:09:25 -0700

    Fix template error.

--- pebble_tool/sdk/templates.py
+++ pebble_tool/sdk/templates.py
@@ -52,7 +52,7 @@ def build(ctx):
             binaries.append({'platform': p, 'app_elf': app_elf})
 
     ctx.set_group('bundle')
-    ctx.pbl_bundle(binaries=binaries, js=ctx.path.ant_glob('src/js/**/*.js'))    ctx.pbl_bundle(binaries=binaries, js=ctx.path.ant_glob('src/js/**/*.js'))
+    ctx.pbl_bundle(binaries=binaries, js=ctx.path.ant_glob('src/js/**/*.js'))
 """
 
 FILE_WSCRIPT_LEGACY2 = """
