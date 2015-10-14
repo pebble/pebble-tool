2dae084e24acaa6cc0d576f5cd923d1e8451c2df
Katharine Berry <katharine@getpebble.com>
2015-10-13 17:36:06 -0700

    Change up the voice tool UI

--- pebble_tool/commands/transcription_server.py
+++ pebble_tool/commands/transcription_server.py
@@ -12,6 +12,12 @@ from .base import PebbleCommand
 
 logger = logging.getLogger("pebble_tool.commands.transcription_server")
 
+mapping = {
+    'connectivity': TranscriptionResult.FailNoInternet,
+    'disabled': SetupResult.FailDisabled,
+    'no-speech-detected': TranscriptionResult.FailSpeechNotRecognized,  # works because there's no mic on qemu.
+}
+
 
 class TranscriptionServer(PebbleCommand):
     ''' Starts a voice server listening for voice transcription requests from the app '''
@@ -19,7 +25,11 @@ class TranscriptionServer(PebbleCommand):
 
     def _send_result(self):
         self._voice_service.send_stop_audio()
-        self._voice_service.send_dictation_result(result=self._result, sentences=[self._words], app_uuid=self._app_uuid)
+        if isinstance(self._error, TranscriptionResult):
+            result = self._error
+        else:
+            result = TranscriptionResult.Success
+        self._voice_service.send_dictation_result(result=result, sentences=[self._words], app_uuid=self._app_uuid)
 
     def _handle_session_setup(self, app_uuid, encoder_info):
         RESULT_DELAY = 4
@@ -28,20 +38,26 @@ class TranscriptionServer(PebbleCommand):
             self._timer.cancel()
         self._app_uuid = app_uuid
 
-        self._voice_service.send_session_setup_result(SetupResult.Success, self._app_uuid)
-        self._timer = threading.Timer(RESULT_DELAY, self._send_result)
-        self._timer.start()
+        if isinstance(self._error, SetupResult):
+            result = self._error
+        else:
+            result = SetupResult.Success
+        self._voice_service.send_session_setup_result(result, self._app_uuid)
+        if result == SetupResult.Success:
+            self._timer = threading.Timer(RESULT_DELAY, self._send_result)
+            self._timer.start()
 
     def _handle_audio_stop(self):
         if self._timer is not None:
             self._timer.cancel()
-        self._voice_service.send_dictation_result(result=self._result, sentences=[self._words], app_uuid=self._app_uuid)
+        self._send_result()
 
     def __call__(self, args):
         super(TranscriptionServer, self).__call__(args)
-        if args.result == 0 and args.transcription is None:
-            raise ValueError("Must provide transcription argument if dictation result is 0 (Success)")
-        self._result = TranscriptionResult(args.result)
+        if args.error is not None:
+            self._error = mapping[args.error]
+        else:
+            self._error = None
         self._voice_service = VoiceService(self.pebble)
         self._timer = None
 
@@ -67,8 +83,9 @@ class TranscriptionServer(PebbleCommand):
     @classmethod
     def add_parser(cls, parser):
         parser = super(TranscriptionServer, cls).add_parser(parser)
-        parser.add_argument('result', type=int, nargs='?', choices=xrange(0, 7),
-                            help='Dictation result code to send')
-        parser.add_argument('-t', '--transcription', type=str,
+        group = parser.add_mutually_exclusive_group(required=True)
+        group.add_argument('transcription', nargs='?', type=str,
                             help="Transcribed message to send in the dictation result")
+        group.add_argument('--error', type=str, nargs='?', choices=mapping.keys(),
+                            help='Error code to respond with, if simulating a failure.')
         return parser
