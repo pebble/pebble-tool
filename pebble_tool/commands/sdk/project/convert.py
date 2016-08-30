e31ac08566ebff10b000dd76292056b94303a14c
Cherie Williams <cherie@pebble.com>
2016-08-30 12:31:08 -0700

    Update convert-project to use sdk JSON maps

--- pebble_tool/commands/sdk/project/convert.py
+++ pebble_tool/commands/sdk/project/convert.py
@@ -1,11 +1,11 @@
 from __future__ import absolute_import, print_function
 
 import collections
-import hashlib
 import json
 import os
 
-from shutil import copy2
+from copy import deepcopy
+from shutil import copy2, copytree, ignore_patterns, move, rmtree
 
 from pebble_tool.commands.sdk.project import SDKProjectCommand
 from pebble_tool.exceptions import OutdatedProjectException, ToolError
@@ -26,7 +26,9 @@ class PblProjectConverter(SDKProjectCommand):
             else:
                 print("No conversion required")
         except OutdatedProjectException:
-            self._convert_project()
+            self._generate_appinfo_from_old_project(os.getcwd())
+            super(PblProjectConverter, self).__call__(args)
+            self._convert_project(move_files=args.move)
             self._convert_to_npm()
             print("Project successfully converted!")
 
@@ -79,33 +81,102 @@ class PblProjectConverter(SDKProjectCommand):
             with open('.gitignore', 'a') as f:
                 f.write("\nnode_modules/\n")
 
-    def _convert_project(self):
-        project_root = os.getcwd()
+    def _convert_project(self, move_files=False):
+        # Map project type naming between projectType and sdk-core's types
+        option_mapping = {
+            "native": "app",
+            "package": "lib",
+            "rocky": "rocky"
+        }
+        option = option_mapping[self.project.project_type]
+
         project_template_path = os.path.join(self.get_sdk_path(), 'pebble', 'common', 'templates')
         if not os.path.exists(project_template_path):
             project_template_path = os.path.join(os.path.dirname(__file__), '..', '..', '..', 'sdk', 'templates')
 
-        self._generate_appinfo_from_old_project(project_root)
+        # Read in conversion mapping JSON file, use fallback of app wscript-update only if none
+        conversion_specification = os.path.join(project_template_path, 'convert.json')
+        conversion_map = {
+            "app": {
+                "update": ["wscript"]
+            }
+        }
+        if os.path.exists(conversion_specification):
+            with open(conversion_specification) as f:
+                conversion_map = json.load(f)
 
-        wscript_path = os.path.join(project_root, "wscript")
+        # Read in template mapping JSON file, use fallback of template folder if none
+        template_specification = os.path.join(project_template_path, 'templates.json')
+        if os.path.exists(template_specification):
+            with open(template_specification) as f:
+                template_map = json.load(f)
 
-        wscript2_hash = hashlib.md5(open(os.path.join(project_template_path, 'wscript_sdk2')).read()).hexdigest()
-        wscript3_hash = hashlib.md5(open(os.path.join(project_template_path, 'wscript')).read()).hexdigest()
-        with open(wscript_path, "r") as f:
-            current_hash = hashlib.md5(f.read()).hexdigest()
+        if "update" in conversion_map[option]:
+            for update_file in conversion_map[option]["update"]:
+                file_path = os.path.join(os.getcwd(), update_file)
+                backup_file = file_path + '.backup'
+                if os.path.exists(file_path):
+                    os.rename(file_path, backup_file)
+                    print("A '{file}' already exists. Backup of {file} saved to {backup}".
+                          format(file=update_file, backup=backup_file))
 
-        if wscript2_hash != current_hash and wscript3_hash != current_hash:
-            print('WARNING: You had modified your wscript and those changes will be lost.\n'
-                  'Saving your old wscript in wscript.backup.')
-            os.rename(wscript_path, wscript_path + '.backup')
+                try:
+                    copy2(template_map["default"][update_file], file_path)
+                except KeyError:
+                    try:
+                        template = template_map[option]["default"][update_file]
+                        copy2(os.path.join(project_template_path, template), file_path)
+                    except KeyError:
+                        raise ToolError("Unable to find file '{}' in available SDK templates.".format(update_file))
+                except TypeError:
+                    try:
+                        copy2(os.path.join(project_template_path, update_file), file_path)
+                    except IOError:
+                        raise ToolError("Unable to find file '{}' in available SDK3 templates".format(update_file))
 
-        print('Generating new 3.x wscript')
-        copy2(os.path.join(project_template_path, 'wscript'), wscript_path)
+        if move_files and "move" in conversion_map[option]:
+            for k, v in conversion_map[option]["move"].iteritems():
+                self._move_path(k, v)
+
         os.system('pebble clean')
+
+    def _move_path(self, old_path, new_path, exclude=[]):
+        full_path = None
+        if isinstance(new_path, dict):
+            for k, v in new_path.iteritems():
+                if k == "":
+                    full_path = v
+                else:
+                    exclude.append(k)
+                    self._move_path(os.path.join(old_path, k), v)
+            else:
+                if full_path:
+                    self._move_path(old_path, full_path, exclude=deepcopy(exclude))
+        else:
+            if not os.path.exists(old_path):
+                print("Path '{}' does not exist and therefore cannot be moved.".format(old_path))
+                return
+
+            print("Moving '{}' to '{}'".format(old_path, new_path))
+            if os.path.isdir(old_path):
+                for item in os.listdir(new_path):
+                    if item in exclude:
+                        continue
+                    if os.path.isdir(item):
+                        copytree(old_path, new_path)
+                        rmtree(os.path.join(old_path, item))
+                    else:
+                        copy2(old_path, new_path)
+                        os.remove(os.path.join(old_path, item))
+            else:
+                os.mkdir(os.path.dirname(new_path))
+                move(old_path, new_path)
 
     @classmethod
     def _generate_appinfo_from_old_project(cls, project_root):
         app_info_path = os.path.join(project_root, "appinfo.json")
+        if not os.path.exists(app_info_path):
+            print("No appinfo.json file exists to convert.")
         with open(app_info_path, "r") as f:
             app_info_json = json.load(f, object_pairs_hook=collections.OrderedDict)
 
@@ -114,3 +185,10 @@ class PblProjectConverter(SDKProjectCommand):
 
         with open(app_info_path, "w") as f:
             json.dump(app_info_json, f, indent=2, separators=(',', ': '))
+
+    @classmethod
+    def add_parser(cls, parser):
+        parser = super(PblProjectConverter, cls).add_parser(parser)
+        parser.add_argument('--move', action='store_true',
+                            help="Update project by moving files that have changed location (experimental).")
+        return parser
