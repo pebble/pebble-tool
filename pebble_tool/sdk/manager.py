4a765c9199857f92ca055d90f08f3218dba904bc
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 05:19:22 +0000

    Adding numeric_owner as keyword arguement

--- pebble_tool/sdk/manager.py
+++ pebble_tool/sdk/manager.py
@@ -126,7 +126,7 @@ class SDKManager(object):
                         if not is_within_directory(path, member_path):
                             raise Exception("Attempted Path Traversal in Tar File")
                 
-                    tar.extractall(path, members, numeric_owner) 
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
                     
                 
                 safe_extract(t, path)
